8ca36cceb7ff948bc589a781d4b3542718ff5a7f
Aniket Kumar <44335648+Aniketkjnv@users.noreply.github.com>
2020-05-26 16:35:33 +0000

    Add files via upload

--- CAMLCASE.C
+++ CAMLCASE.C
@@ -2,15 +2,13 @@
 	in a given string S. */
 	
 #include<stdio.h>
-#include<conio.h>
-
+#include<string.h>
 int camel_case(char*);
 
-void main()
+int main()
 {
 	char str[20];
 	int n,i=0,j;
-	clrscr();
 	{
 	
 		printf("This is a program to find the no of upper case alphabets");
@@ -25,19 +23,23 @@ void main()
 			printf("\nThe no of upper case alphabets is : %d\n ",n);
 			i++;
 		}
-		getch();
 	}
 }
 
 int camel_case(char str[])
 {
-	int i,j=20,count=0;
+	int i,j,count=0;
+	j=strlen(str);
 
 	for(i=0;i<j;i++)
 	{
 		if(str[i]>=65&&str[i]<=90)//comparing to the ASCII values
 		{
-			count=count+1; //counting the number of alphabets
+			count=count+1; //counting the number of Camel Case letters
+		}
+		else
+		{
+			count=count;
 		}
 	}
 	return count;
